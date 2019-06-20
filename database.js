89514747043ba3f27603e868c52ed650fefecbd7
Loliticos <47262825+Loliticos@users.noreply.github.com>
2019-06-20 19:51:25 -0300

    🎉 Adicionar nova classe.  🎉

--- database.js
+++ database.js
@@ -1,23 +1,24 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-mongoose.connect('mongodb+srv://loliticos:list@cluster0-ghogp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
 
 const User = new Schema({
-	_id: { type: String, required: true },
-	money: { type: Number, default: 0 },
-	trabalho: { type: Number, default: 0 }
+  _id: { type: String, required: true },
+  money: { type: Number, default: 0 },
+  trabalho: { type: Number, default: 0 }
 })
 
 const Guild = new Schema({
-	_id: { type: String, required: true },
-	prefix: { type: String, default: 'y!' },
-	language: { type: String, default: 'pt-br' },
+  _id: { type: String, required: true },
+  prefix: { type: String, default: 'y!' },
+  language: { type: String, default: 'pt' },
   welcomeMessage: { type: String, default: false },
   welcomeChannel: { type: String, default: false },
   logChannel: { type: String, default: false },
   leftMessage: { type: String, default: false },
-  leftChannel: { type: String, default: false }
+  leftChannel: { type: String, default: false },
+  autoRole: { type: Array, default: false }
 })
 
 
